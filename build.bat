934f73cc8b9772ce7193792468da37a6654935f5
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2022-11-11 23:49:26 -0500

    Add debug symbols to optimized build

--- build.bat
+++ build.bat
@@ -7,4 +7,4 @@ copy "..\Odin\vendor\sdl2\ttf\SDL2_ttf.dll" "bin\SDL2_ttf.dll"
 copy "fonts\*.*" "bin"
 REM odin build src -subsystem:windows -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed
 REM odin build src -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed -keep-temp-files
-odin build src -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed -keep-temp-files
+odin build src -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed -debug -keep-temp-files
