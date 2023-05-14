91e60f8af48068acc3681bbe925c320a20a1a457
Colin Davidson <colrdavidson@gmail.com>
2023-05-13 20:13:08 -0700

    move packaging to seperate script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-rm spall*.zip
 rm -rf bin
 mkdir bin
 
@@ -8,11 +7,4 @@ elif [ "$1" = "opt" ]; then
 	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed
 else
 	odin build src -collection:formats=formats -out:bin/spall -debug -keep-temp-files
-fi
-
-if [ "$1" = "release" ]; then
-	cp resources/* bin/.
-
-	spall_date=$(date +'%m_%d_%Y')
-	zip -r spall_native_$spall_date.zip bin
 fi
