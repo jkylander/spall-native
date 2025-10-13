fa41422e23ee18599e9389905731dec469305a49
Colin Davidson <colrdavidson@gmail.com>
2025-10-12 20:29:09 -0700

    print fix dylib issues

--- fix_dylib.sh
+++ fix_dylib.sh
@@ -1,5 +1,7 @@
+set -x
+
 DYLIB_NAME=$1
 BIN_PATH=$2
 
-DYLIB_PATH=$(otool -L $BIN_PATH | grep -m 1 $DYLIB_NAME | awk -F '.dylib' '{print $1}' | awk '{$1=$1};1')
+DYLIB_PATH=$(otool -L $BIN_PATH | grep -m 1 $DYLIB_NAME | awk -F '.dylib' '{print $1".dylib"}' | awk '{$1=$1};1')
 install_name_tool -change $DYLIB_PATH @executable_path/../Frameworks/$DYLIB_NAME $BIN_PATH
